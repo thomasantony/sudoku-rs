e83347f7b9251d852db83ab49deb24946fd0ff71
Thomas Antony <tantony@purdue.edu>
2020-04-11 20:43:07 -0500

    Initial varisat implementation -- not working

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,89 @@
 mod utils;
 use varisat::{Var, Lit};
 use varisat::solver::Solver;
-use varisat::ExtendFormula;
+use itertools::{iproduct};
+use varisat::{CnfFormula, ExtendFormula};
 
 fn lit_from_value(row: usize, col: usize, value: usize) -> Lit {
     Var::from_index(row * 9 * 9 + col * 9 + value).lit(true)
 }
+
+fn exactly_once(literals: &Vec<Lit>) -> CnfFormula{
+    let mut formula = CnfFormula::new();
+    
+    formula.add_clause(literals);
+    for (i, lit1) in literals.iter().enumerate() {
+        for lit2 in literals[i+1..].iter() {
+            formula.add_clause(&[!lit1.clone(), !lit2.clone()]);
+        }
+    }
+    formula
+}
+fn literals_from_board(board: &utils::SudokuGrid) -> Vec<Lit>
+{
+    board.iter().map(|((row, col), cell)| {
+        match cell {
+            Some(value) => {
+                println!("Adding value {} at {},{}", value, row, col);
+                Some(lit_from_value(*row, *col, *value-1))
+            },
+            _ => None
+        }
+    })
+    .flatten()
+    .collect()
+}
 fn main() {
     // // let boxes = utils::create_grid_9x9_keys();
-    // let grid_str = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..";
+    let grid_str = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..";
 
     // // println!("{:?}", utils::parse_grid(grid_str.to_string()));
-    // let grid = utils::parse_grid(grid_str.to_string());
-    // utils::display_grid(grid);
+    let grid = utils::parse_grid(grid_str.to_string());
+    utils::display_grid(&grid);
 
     let mut solver = Solver::new();
 
     // Each row has all numbers from 1..9 exactly once
-    for row in 0..9 {
-        for value in 0..9 {
-            let mut literals: Vec<Lit> = Vec::new();
-            for col in 0..9 {
-                literals.push(lit_from_value(row, col, value));
-            }
-            println!("{}:{} {:?}", row, value, &literals);
-            solver.add_clause(&literals);
+    for (row, value) in iproduct!(0..9, 0..9)
+    {
+        let mut literals: Vec<Lit> = Vec::new();
+        for col in 0..9 {
+            literals.push(lit_from_value(row, col, value));
         }
+        solver.add_formula(& exactly_once(&literals));
     }
     // Each column has all numbers from 1..9 exactly once
-    for col in 0..9 {
-        for value in 0..9 {
-            let mut literals: Vec<Lit> = Vec::new();
-            for row in 0..9 {
-                literals.push(lit_from_value(row, col, value));
-            }
-            solver.add_clause(literals.as_slice());
+    for (col, value) in iproduct!(0..9, 0..9)
+    {
+        let mut literals: Vec<Lit> = Vec::new();
+        for row in 0..9 {
+            literals.push(lit_from_value(row, col, value));
         }
+        solver.add_clause(literals.as_slice());
     }
 
     // Each box has all numbers from 1..9 exactly once
-    for col in 0..9 {
-        for value in 0..9 {
+    for value in 0..9 
+    {
+        for (r, c) in iproduct!(&[0,3,6], &[0,3,6])
+        {
             let mut literals: Vec<Lit> = Vec::new();
-            for row in 0..9 {
+            for (rr, cc) in iproduct!(&[0, 1, 2], 
+                                      &[0, 1, 2])
+            {
+                let row = (r + rr) as usize;
+                let col = (c + cc) as usize;
                 literals.push(lit_from_value(row, col, value));
             }
             solver.add_clause(literals.as_slice());
         }
     }
+    // Add in pre-filled boxes
+    let board_lit = literals_from_board(&grid);
+    println!("{:?}", &board_lit);
+    solver.add_clause(& board_lit);
+
+    // let solution = solver.solve().unwrap();
+    let model = solver.model().unwrap(); // None if solve didn't return Ok(true)
+    println!("Solution: {:?}", model);
 }
--- src/utils.rs
+++ src/utils.rs
@@ -3,50 +3,47 @@ use std::collections::HashMap;
 use std::iter::FromIterator;
 
 
-pub type SudokuGrid = HashMap<(char, char), Option<u32>>;
-static ROWS: &str = "ABCDEFGHI";
-static COLS: &str = "123456789";
+pub type SudokuGrid = HashMap<(usize, usize), Option<usize>>;
 
-pub fn grid_9x9_keys(rows: &'static str, cols: &'static str) -> impl Iterator<Item=(char, char)>
+pub fn grid_9x9_keys() -> impl Iterator<Item=(usize, usize)>
 {
-    let boxes = iproduct!(rows.chars(), cols.chars());
+    let boxes = iproduct!(0..9, 0..9);
     boxes
 }
 pub fn parse_grid(s: String) -> SudokuGrid
 {
-    let boxes = grid_9x9_keys(ROWS, COLS);
+    let boxes = grid_9x9_keys();
     let values = s.chars().map(|c| match c {
         '.' => None,
-        c => c.to_digit(10),
+        c => c.to_digit(10).map(|d| d as usize),
     });
 
     SudokuGrid::from_iter(boxes.zip(values))
 }
 
-pub fn display_grid(g: SudokuGrid)
+pub fn display_grid(g: &SudokuGrid)
 {
     // width = 1+max(len(values[s]) for s in boxes)
     // let width = 2;
     let line = std::iter::repeat("-").take(9).collect::<String>();
     let line = std::iter::repeat(line).take(3).collect::<Vec<String>>().join("+");
-    for r in ROWS.chars()
+    for r in 0..9
     {
-        let value_str = COLS.chars()
-                            .map(|c| (r, c))
+        let value_str = (0..9).map(|c| (r, c))
                             .map(|k|
                             {
                                 let num = g[&k];
                                 let mut num_str = num.map_or(" . ".to_string(), |num|{
                                     format!("{:^3}", num)
                                 });
-                                if k.1 == '3' || k.1 == '6'
+                                if k.1 == 2 || k.1 == 5
                                 {
                                     num_str += "|";
                                 }
                                 num_str
                             }).collect::<String>();
         println!("{}", value_str);
-        if r == 'C' || r == 'F'
+        if r == 2 || r == 5
         {
             println!("{}", line);
         }
